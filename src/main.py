39e112b6454265f740b4b0531d89a797d0cda7b1
Mr.Quantum <darshanpatelgdh@gmail.com>
2025-03-22 00:45:09 +0530

    Updated solution

--- src/main.py
+++ src/main.py
@@ -1,14 +1,42 @@
-def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
+import math
+
+def main(input_file_name="testcase.txt", output_file_name="output.txt"):
     input_file = open(input_file_name, "r")
     output_file = open(output_file_name, "w")
 
-    first_line = input_file.readline().strip()
-    first_line = first_line.split(";")
+    minmap = {}
+    maxmap = {}
+    meanmap = {}
 
-    output_file.write(f"{first_line[0]}={first_line[1]}/{1}/{1}\n")
+    for line in input_file:
+        li = line.strip().split(';')
+        key, value = li[0], float(li[1])
+        if key in minmap:
+            minmap[key] = min(minmap[key], value)
+        else:
+            minmap[key] = value
+        if key in maxmap:
+            maxmap[key] = max(maxmap[key], value)
+        else:
+            maxmap[key] = value
+        if key in meanmap:
+            meanmap[key][0] += value
+            meanmap[key][1] += 1
+        else:
+            meanmap[key] = [value, 1]
+
+    for key in meanmap:
+        meanmap[key] = meanmap[key][0] / meanmap[key][1]
+
+    for key in sorted(minmap.keys()):
+        minX = math.ceil(minmap[key] * 10) / 10
+        meanX = math.ceil(meanmap[key] * 10) / 10
+        maxX = math.ceil(maxmap[key] * 10) / 10
+        output_file.write(f"{key}={minX}/{meanX}/{maxX}\n")
 
     output_file.close()
     input_file.close()
+
 
 if __name__ == "__main__":
     main()
