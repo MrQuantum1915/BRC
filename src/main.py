c7920b9ddf67307f51744995a73b17b9f58ee80a
Mr.Quantum <darshanpatelgdh@gmail.com>
2025-03-22 03:12:19 +0530

    update

--- src/main.py
+++ src/main.py
@@ -41,31 +41,89 @@
 
 
 
-import math
+# import math
 
-def main(input_file_name="testcase.txt", output_file_name="output.txt"):
+# def main(input_file_name="testcase.txt", output_file_name="output.txt"):
     
-    values = {}
+#     values = {}
+
+#     with open(input_file_name, "r") as input_file:
+#         for line in input_file:
+#             key, value = line.strip().split(';')
+#             value = float(value)
+
+#             if key not in values:
+#                 values[key] = [value, value, value, 1]
+#             else:
+#                 values[key][0] = min(values[key][0], value)
+#                 values[key][1] = max(values[key][1], value)
+#                 values[key][2] += value
+#                 values[key][3] += 1
+
+#     with open(output_file_name, "w") as output_file:
+#         for key in sorted(values.keys()):
+#             minX = math.ceil(values[key][0] * 10) / 10
+#             meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10
+#             maxX = math.ceil(values[key][1] * 10) / 10
+#             output_file.write(f"{key}={minX}/{meanX}/{maxX}\n")
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+# import math
+# from collections import defaultdict
+
+# def main(input_file_name="testcase.txt", output_file_name="output.txt"):
+#     values = defaultdict(lambda: [float('inf'), float('-inf'), 0, 0])
+
+#     with open(input_file_name, "r") as input_file:
+#         for line in input_file:
+#             key, value = line.strip().split(';')
+#             value = float(value)
+
+#             values[key][0] = min(values[key][0], value) 
+#             values[key][1] = max(values[key][1], value) 
+#             values[key][2] += value                     
+#             values[key][3] += 1                         
+
+#     with open(output_file_name, "w") as output_file:
+#         for key in sorted(values.keys()):
+#             minX = math.ceil(values[key][0] * 10) / 10
+#             meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10
+#             maxX = math.ceil(values[key][1] * 10) / 10
+#             output_file.write(f"{key}={minX}/{meanX}/{maxX}\n")
+
+# if __name__ == "__main__":
+#     main()
+    
+    
+    
+import math
+from collections import defaultdict
+
+def process_file(input_file_name="testcase.txt", output_file_name="output.txt"):
+    values = defaultdict(lambda: [float('inf'), float('-inf'), 0, 0])
 
     with open(input_file_name, "r") as input_file:
         for line in input_file:
-            key, value = line.strip().split(';')
-            value = float(value)
+            key, value = line.strip().split(';')  
+            value = float(value) 
 
-            if key not in values:
-                values[key] = [value, value, value, 1]
-            else:
-                values[key][0] = min(values[key][0], value)
-                values[key][1] = max(values[key][1], value)
-                values[key][2] += value
-                values[key][3] += 1
+            values[key][0] = min(values[key][0], value) 
+            values[key][1] = max(values[key][1], value) 
+            values[key][2] += value                     
+            values[key][3] += 1                         
 
     with open(output_file_name, "w") as output_file:
         for key in sorted(values.keys()):
             minX = math.ceil(values[key][0] * 10) / 10
-            meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10
-            maxX = math.ceil(values[key][1] * 10) / 10
+            meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10 if values[key][3] > 0 else 0
+            maxX = math.ceil(values[key][1] * 10) / 10 
             output_file.write(f"{key}={minX}/{meanX}/{maxX}\n")
 
 if __name__ == "__main__":
-    main()
+    process_file("testcase.txt", "output.txt")
